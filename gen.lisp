49eb4264fae3f887cbc6621ba3ec54a65976ce0a
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-08-08 05:56:06 +0200

    do interpolation with barycentric coordinates, not sure if this
    works

--- gen.lisp
+++ gen.lisp
@@ -52,18 +52,99 @@ is replaced with replacement."
 		 (include <array>)
 	       (include <stdint.h>)
 	       (include <stddef.h>)
+	       (include <complex>)
+	       
 	       (raw "using namespace std;")
 	
 
 	       #+nil ,@(dox :brief ""
-		      :usage ""
-		      :params '((arguments "")
-				)
-		      :return "")
+			    :usage ""
+			    :params '((arguments "")
+				      )
+			    :return "")
 
 	       (function (fuzz_me ((data :type "const uint8_t*")
 				   (data_size :type "size_t"))
 				  bool)
+			 (return (&& (&& (<= 3 data_size)
+					 (&& (== (char #\F) (aref data 0))
+					     (== (char #\U) (aref data 1))))
+				     (&& (== (char #\Z) (aref data 2))
+					 (== (char #\Z) (aref data 3))))))
+	       ;; http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html
+	       ;; https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates
+	       ;; https://classes.soe.ucsc.edu/cmps160/Fall10/resources/barycentricInterpolation.pdf
+	       ;; http://answers.unity3d.com/questions/383804/calculate-uv-coordinates-of-3d-point-on-plane-of-m.html
+	       (function (rasterize_triangle (,@(loop for i in '(a b c) collect
+						     `(,i :type complex<float>))
+						,@(loop for i below 3 collect
+						       `(,(format nil "f~d" i) :type float)
+						       )
+						(image :type "array<array<float,100>,200>&"))
+					     void)
+			 (let ((image_max_x :type float :ctor (funcall image.size))
+			       (image_max_y :type float :ctor (funcall "image[0].size"))
+			       ,@ (loop for typ in '(min max)
+				     appending
+				       (loop for coord in '(x y) and fun in '(real imag) collect
+					    `(,(format nil "~a_~a" typ coord)
+					       :ctor (funcall ,typ
+							      ,(case typ
+								 (min `0s0)
+								 (max (format nil "image_max_~a" coord) ))
+							      (funcall ,typ
+							       (funcall ,typ
+									(funcall ,fun a)
+									(funcall ,fun b))
+							       (funcall ,fun c)
+							       )
+							      )))))
+			   
+			   (let ((v0 :ctor (- b a))
+				 (v1 :ctor (- c a))
+				 
+				 ,@ (remove-if #'null
+				      (loop for i below 2 appending
+					   (loop for j below 2 collect
+						(when (<= i j)
+						  `(,(format nil "d~d~d" i j) :ctor (funcall real (*
+												   ,(format nil "v~d" i)
+												   ,(format nil "v~d" j))))))))
+				 (denom_ :ctor (/ 1s0
+						  (- (* d00 d11)
+						     (* d01 d01)))))
+			     
+			     (for ((y min_y) (<= y max_y) (+= y 1))
+				  (for ((x min_x) (<= x max_x) (+= x 1))
+				       (let ((p :type complex<float> :ctor (comma-list x y))
+					     (v2 :ctor (- p a))
+					     ,@ (remove-if #'null
+						  (loop for i below 3 appending
+						       (loop for j below 3 collect
+							    (when (and (<= i j) (or (= i 2) (= j 2)))
+							      `(,(format nil "d~d~d" i j) :ctor (funcall real (*
+													       ,(format nil "v~d" i)
+													       ,(format nil "v~d" j))))))))
+					     (v :ctor (* denom_ (- (* d11 d02)
+								   (* d01 d12))))
+					     (w :ctor (* denom_ (- (* d00 d12)
+								   (* d01 d02))))
+					     (u :ctor (- 1s0 v w))
+					     )
+					 (setf (aref image y x)
+					       (+ (* u f0)
+						  (* v f1)
+						  (* w f2)))
+					 ))))))
+	       (function (lerp ((v0 :type float)
+				(v1 :type float)
+				(x :type float))
+			       float)
+			 (return (+ (* (- 1 x) v0)
+				    (* x v1))))
+	       (function (fuzz_me2 ((data :type "const uint8_t*")
+				    (data_size :type "size_t"))
+				   bool)
 			 (return (&& (&& (<= 3 data_size)
 					 (&& (== (char #\F) (aref data 0))
 					     (== (char #\U) (aref data 1))))
@@ -78,3 +159,4 @@ is replaced with replacement."
   (write-source "stage/cl-gen-fuzz/source/main" "cpp" code))
 
 
+;;] http://releases.llvm.org/3.9.0/docs/LibFuzzer.html
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,49 @@
 #include <array>
+#include <complex>
 #include <stddef.h>
 #include <stdint.h>
 using namespace std;
 bool fuzz_me(const uint8_t *data, size_t data_size) {
+  return (((3 <= data_size) && (('F' == data[0]) && ('U' == data[1]))) &&
+          (('Z' == data[2]) && ('Z' == data[3])));
+}
+void rasterize_triangle(complex<float> a, complex<float> b, complex<float> c,
+                        float f0, float f1, float f2,
+                        array<array<float, 100>, 200> &image) {
+  {
+    float image_max_x(image.size());
+    float image_max_y(image[0].size());
+    auto min_x(min((0.0e+0f), min(min(real(a), real(b)), real(c))));
+    auto min_y(min((0.0e+0f), min(min(imag(a), imag(b)), imag(c))));
+    auto max_x(max(image_max_x, max(max(real(a), real(b)), real(c))));
+    auto max_y(max(image_max_y, max(max(imag(a), imag(b)), imag(c))));
+    {
+      auto v0((b - a));
+      auto v1((c - a));
+      auto d00(real((v0 * v0)));
+      auto d01(real((v0 * v1)));
+      auto d11(real((v1 * v1)));
+      auto denom_(((1.e+0f) / ((d00 * d11) - (d01 * d01))));
+      for (auto y = min_y; (y <= max_y); y += 1) {
+        for (auto x = min_x; (x <= max_x); x += 1) {
+          {
+            complex<float> p(x, y);
+            auto v2((p - a));
+            auto d02(real((v0 * v2)));
+            auto d12(real((v1 * v2)));
+            auto d22(real((v2 * v2)));
+            auto v((denom_ * ((d11 * d02) - (d01 * d12))));
+            auto w((denom_ * ((d00 * d12) - (d01 * d02))));
+            auto u(((1.e+0f) - v - w));
+            image[y][x] = ((u * f0) + (v * f1) + (w * f2));
+          }
+        }
+      }
+    }
+  }
+}
+float lerp(float v0, float v1, float x) { return (((1 - x) * v0) + (x * v1)); }
+bool fuzz_me2(const uint8_t *data, size_t data_size) {
   return (((3 <= data_size) && (('F' == data[0]) && ('U' == data[1]))) &&
           (('Z' == data[2]) && ('Z' == data[3])));
 }
